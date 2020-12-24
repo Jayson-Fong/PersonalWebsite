39dbea34de00d2b78a367bd539ef3d8eb4617935
Jayson Fong <jaysonf1007@gmail.com>
2020-12-24 12:01:28 -0500

    Added More Info

--- assets/js/process.js
+++ assets/js/process.js
@@ -2,15 +2,18 @@
  * Opens a new window depending on user input.
  * Room for Improvement: Processing Server to Prevent Snooping
  */
-window.onload = onPageLoad;
-document.getElementById('invitation').addEventListener('input', onInvitationInput);
-document.getElementById('invitation').addEventListener('keyup', function(event) {
-    onKeyUp(event);
-});
+'use strict';
+
+function processInvitation(invitationCode) {
+    switch (invitationCode) {
+    case 'CST-0A1': 
+        window.open('https://forms.office.com/Pages/ResponsePage.aspx?id=-VzmfS4_Z06kaZZbDQhfuzsjwJ6dgCNKqEQN6AcZFihUMFhBUlBUVVFCMUJMVVVYOEdDVzUxUTBVRS4u', '_blank');
+        break;
+    }
+}
 
 function onPageLoad() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const invitationCode = urlParams.get('invitation');
+    var invitationCode = (new URLSearchParams(window.location.search)).get('invitation');
     document.getElementById('invitation').value = invitationCode;
     processInvitation(invitationCode);
 }
@@ -20,11 +23,39 @@ function onInvitationInput() {
 }
 
 function onKeyUp(event) {
-    if (event.keyCode === 13) onInvitationInput();
-}
-
-function processInvitation(invitationCode) {
-    switch(invitationCode) {
-        case 'CST-0A1': window.open('https://forms.office.com/Pages/ResponsePage.aspx?id=-VzmfS4_Z06kaZZbDQhfuzsjwJ6dgCNKqEQN6AcZFihUMFhBUlBUVVFCMUJMVVVYOEdDVzUxUTBVRS4u', '_blank');
+    if (event.keyCode === 0b1101) {
+        onInvitationInput();
     }
 }
+
+function toggleCollapsible(element) {
+    var collapsible = document.getElementById(element.getAttribute('data-collapsible-id'));
+    collapsible.hidden = !collapsible.hidden;
+    var carets = (element.getElementsByClassName('collapsible-button')[0b0]).getElementsByTagName('i');
+    hideElement(carets[0b0], !collapsible.hidden);
+    hideElement(carets[0b1], collapsible.hidden);
+}
+
+function hideElement(element, show) {
+    if (show) {
+        element.style.display = 'inline';
+        element.hidden = false;
+    } else {
+        element.style.display = 'none';
+        element.hidden = true;
+    }
+}
+
+window.onload = onPageLoad;
+document.getElementById('invitation').addEventListener('input', onInvitationInput);
+document.getElementById('invitation').addEventListener('keyup', function(event) {
+    onKeyUp(event);
+});
+
+var collapsibleButtons = document.getElementsByClassName('collapsible-button-link');
+Array.prototype.forEach.call(collapsibleButtons, function(element) {
+    toggleCollapsible(element);
+    element.addEventListener('click', function(event) {
+        toggleCollapsible(element);
+    });
+});
