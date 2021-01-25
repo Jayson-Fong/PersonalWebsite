8b472fdc9958e80a644b58e33158a1a016bccba1
Jayson Fong <fong.jayson@gmail.com>
2021-01-25 07:54:56 -0500

    Removed Invitation System

--- assets/js/process.js
+++ assets/js/process.js
@@ -4,30 +4,6 @@
  */
 'use strict';
 
-function processInvitation(invitationCode) {
-    switch (invitationCode) {
-    case 'CST-0A1': 
-        window.open('https://forms.office.com/Pages/ResponsePage.aspx?id=-VzmfS4_Z06kaZZbDQhfuzsjwJ6dgCNKqEQN6AcZFihUMFhBUlBUVVFCMUJMVVVYOEdDVzUxUTBVRS4u', '_blank');
-        break;
-    }
-}
-
-function onPageLoad() {
-    var invitationCode = (new URLSearchParams(window.location.search)).get('invitation');
-    document.getElementById('invitation').value = invitationCode;
-    processInvitation(invitationCode);
-}
-
-function onInvitationInput() {
-    processInvitation(document.getElementById('invitation').value);
-}
-
-function onKeyUp(event) {
-    if (event.keyCode === 0b1101) {
-        onInvitationInput();
-    }
-}
-
 function toggleCollapsible(element) {
     var collapsible = document.getElementById(element.getAttribute('data-collapsible-id'));
     collapsible.hidden = !collapsible.hidden;
@@ -45,12 +21,6 @@ function hideElement(element, show) {
         element.hidden = true;
     }
 }
-
-window.onload = onPageLoad;
-document.getElementById('invitation').addEventListener('input', onInvitationInput);
-document.getElementById('invitation').addEventListener('keyup', function(event) {
-    onKeyUp(event);
-});
 
 var collapsibleButtons = document.getElementsByClassName('collapsible-button-link');
 Array.prototype.forEach.call(collapsibleButtons, function(element) {
